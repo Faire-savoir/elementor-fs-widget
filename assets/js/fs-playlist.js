2a226f224bcb6da42c92689273c3f2a561ebc066
Loubal70 <63310494+Loubal70@users.noreply.github.com>
2021-04-19 18:05:29 +0200

    Indentation

--- assets/js/fs-playlist.js
+++ assets/js/fs-playlist.js
@@ -1,58 +1,58 @@
-jQuery( window ).on( 'elementor/frontend/init', () => {
+jQuery(window).on('elementor/frontend/init', () => {
 
-  const initialize_maps  = function( $scope, $ ) {
-    $('.fs-widget-leaflet-map-playlist').each(function(){
-      if ( !$(this).hasClass('fs-playlist-loaded') ) {
+  const initialize_maps = function ($scope, $) {
+    $('.fs-widget-leaflet-map-playlist').each(function () {
+      if (!$(this).hasClass('fs-playlist-loaded')) {
 
         $(this).addClass('fs-playlist-loaded');
         var id = $(this).attr('id');
-        if (id !== undefined){
+        if (id !== undefined) {
           // init vars
           var lat = $(this).attr('data-map-lat') || 50.6333;
           var lon = $(this).attr('data-map-lon') || 3.0619;
           var zoom = $(this).attr('data-zoom') || 13;
-          var style = $(this).attr('data-map-design') || 'osm';
+          var style = $(this).attr('data-map-design') || 'osm';
           var fit_bounds = 'yes';
           var marker_numbering = $(this).attr('data-marker-numbering');
 
-          var elementorMap = L.map(id).setView([lat,lon],zoom);
+          var elementorMap = L.map(id).setView([lat, lon], zoom);
           elementorMap.scrollWheelZoom.disable();
           elementorMap.gestureHandling.enable();
           elementorMap.dragging.enable();
 
-          switch (style){
-            case 'google-roadmap' :
+          switch (style) {
+            case 'google-roadmap':
               var layer = new L.Google('ROADMAP');
               break;
-            case 'google-satellite' :
+            case 'google-satellite':
               var layer = new L.Google('');
               break;
-            case 'google-terrain' :
+            case 'google-terrain':
               var layer = new L.Google('TERRAIN');
               break;
-            case 'google-hybrid' :
+            case 'google-hybrid':
               var layer = new L.Google('HYBRID');
               break;
             case 'mapbox-street':
               var layer = L.tileLayer('https://a.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-                  attribution: '',
-                  id: 'mapbox.streets',
-                  accessToken: 'pk.eyJ1IjoiZmFpcmUtc2F2b2lyIiwiYSI6ImNqcDQ3cTdqOTAxcGMzeG1ranV2NDlvb28ifQ.J-08viX3_VpEhkEg97VB0g',
-                  maxZoom: 18,
+                attribution: '',
+                id: 'mapbox.streets',
+                accessToken: 'pk.eyJ1IjoiZmFpcmUtc2F2b2lyIiwiYSI6ImNqcDQ3cTdqOTAxcGMzeG1ranV2NDlvb28ifQ.J-08viX3_VpEhkEg97VB0g',
+                maxZoom: 18,
               });
               break;
             case 'mapbox-satellite':
               var layer = L.tileLayer('https://a.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-                  attribution: '',
-                  id: 'mapbox.satellite',
-                  accessToken: 'pk.eyJ1IjoiZmFpcmUtc2F2b2lyIiwiYSI6ImNqcDQ3cTdqOTAxcGMzeG1ranV2NDlvb28ifQ.J-08viX3_VpEhkEg97VB0g',
-                  maxZoom: 18,
+                attribution: '',
+                id: 'mapbox.satellite',
+                accessToken: 'pk.eyJ1IjoiZmFpcmUtc2F2b2lyIiwiYSI6ImNqcDQ3cTdqOTAxcGMzeG1ranV2NDlvb28ifQ.J-08viX3_VpEhkEg97VB0g',
+                maxZoom: 18,
               });
               break;
             case 'osm':
               var layer = L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-                  attribution: '',
-                  maxZoom: 19
+                attribution: '',
+                maxZoom: 19
               });
               break;
           }
@@ -60,7 +60,7 @@ jQuery( window ).on( 'elementor/frontend/init', () => {
 
           var markers_array = [];
           var markers_group = L.featureGroup();
-          $(this).find('.point').each(function(idx, obj){
+          $(this).find('.point').each(function (idx, obj) {
 
             var marker_id = $(this).attr('data-id');
             var marker_lat = $(this).attr('data-lat');
@@ -68,21 +68,21 @@ jQuery( window ).on( 'elementor/frontend/init', () => {
             var marker_style = $(this).attr('data-marker');
 
 
-            if (marker_id !== '' && marker_lat !== '' && marker_lon !== ''){
+            if (marker_id !== '' && marker_lat !== '' && marker_lon !== '') {
               var popup = $(this).html()
 
               var iconDiv = L.divIcon({
-                iconSize:[48,58],
-                iconAnchor:[24,58],
-                popupAnchor:[0,-60],
-                className:'marker marker-id-'+marker_id+' '+((marker_style != '' && marker_style != undefined)?'marker-'+marker_style:''),
-                html:'<div class="pin"><span class="content">'+((marker_numbering == 'yes')?marker_id:'')+'<span></div>'
+                iconSize: [48, 58],
+                iconAnchor: [24, 58],
+                popupAnchor: [0, -60],
+                className: 'marker marker-id-' + marker_id + ' ' + ((marker_style != '' && marker_style != undefined) ? 'marker-' + marker_style : ''),
+                html: '<div class="pin"><span class="content">' + ((marker_numbering == 'yes') ? marker_id : '') + '<span></div>'
               })
 
-              var marker = new L.marker([marker_lat,marker_lon],{
-                icon : iconDiv,
+              var marker = new L.marker([marker_lat, marker_lon], {
+                icon: iconDiv,
               });
-              if (popup !== undefined){
+              if (popup !== undefined) {
                 marker.bindPopup(popup);
               }
               markers_array.push(marker);
@@ -91,31 +91,33 @@ jQuery( window ).on( 'elementor/frontend/init', () => {
           });
           elementorMap.addLayer(markers_group);
 
-          if(markers_array.length && fit_bounds == 'yes'){
+          if (markers_array.length && fit_bounds == 'yes') {
             var corners = markers_group.getBounds();
-            elementorMap.fitBounds(corners,{padding: [50,50]});
+            elementorMap.fitBounds(corners, {
+              padding: [50, 50]
+            });
           }
         }
       }
 
     });
   };
-  elementorFrontend.hooks.addAction( 'frontend/element_ready/fs-widget-playlist.default', initialize_maps );
-} );
+  elementorFrontend.hooks.addAction('frontend/element_ready/fs-widget-playlist.default', initialize_maps);
+});
 
-jQuery(function($){
-  $(document).ready(function(){
-    $('.see_more_offers').click(function(ev){
+jQuery(function ($) {
+  $(document).ready(function () {
+    $('.see_more_offers').click(function (ev) {
       ev.preventDefault();
       $(this).closest('.list').find('.more_offers').toggle('slow');
-      if($(this).val() == 'J\'en veux plus'){
+      if ($(this).val() == 'J\'en veux plus') {
         $(this).val('J\'en veux moins');
-      }else {
+      } else {
         $(this).val('J\'en veux plus');
       }
     });
   });
-  $(window).load(function(){
+  $(window).load(function () {
     $('.map_side').addClass('map_loaded');
   });
 });
--- assets/js/fs-widget-leaflet-map.js
+++ assets/js/fs-widget-leaflet-map.js
@@ -1,57 +1,57 @@
-jQuery( window ).on( 'elementor/frontend/init', () => {
+jQuery(window).on('elementor/frontend/init', () => {
 
-  const initialize_maps  = function( $scope, $ ) {
-    $('.fs-widget-leaflet-map').each(function(){
-      if ( !$(this).hasClass('fs-leaflet-map-loaded') ) {
+  const initialize_maps = function ($scope, $) {
+    $('.fs-widget-leaflet-map').each(function () {
+      if (!$(this).hasClass('fs-leaflet-map-loaded')) {
 
         $(this).addClass('fs-leaflet-map-loaded');
         var id = $(this).attr('id');
-        if (id !== undefined){
+        if (id !== undefined) {
           // init vars
           var lat = $(this).attr('data-map-lat') || 50.6333;
           var lon = $(this).attr('data-map-lon') || 3.0619;
           var zoom = $(this).attr('data-zoom') || 15;
-          var style = $(this).attr('data-map-design') || 'osm';
+          var style = $(this).attr('data-map-design') || 'osm';
           var fit_bounds = $(this).attr('data-fit-bounds');
           var marker_numbering = $(this).attr('data-marker-numbering');
 
-          var elementorMap = L.map(id).setView([lat,lon],zoom);
+          var elementorMap = L.map(id).setView([lat, lon], zoom);
           elementorMap.scrollWheelZoom.disable();
           elementorMap.gestureHandling.enable();
 
-          switch (style){
-            case 'google-roadmap' :
+          switch (style) {
+            case 'google-roadmap':
               var layer = new L.Google('ROADMAP');
               break;
-            case 'google-satellite' :
+            case 'google-satellite':
               var layer = new L.Google('');
               break;
-            case 'google-terrain' :
+            case 'google-terrain':
               var layer = new L.Google('TERRAIN');
               break;
-            case 'google-hybrid' :
+            case 'google-hybrid':
               var layer = new L.Google('HYBRID');
               break;
             case 'mapbox-street':
               var layer = L.tileLayer('https://a.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-                  attribution: '',
-                  id: 'mapbox.streets',
-                  accessToken: 'pk.eyJ1IjoiZmFpcmUtc2F2b2lyIiwiYSI6ImNqcDQ3cTdqOTAxcGMzeG1ranV2NDlvb28ifQ.J-08viX3_VpEhkEg97VB0g',
-                  maxZoom: 18,
+                attribution: '',
+                id: 'mapbox.streets',
+                accessToken: 'pk.eyJ1IjoiZmFpcmUtc2F2b2lyIiwiYSI6ImNqcDQ3cTdqOTAxcGMzeG1ranV2NDlvb28ifQ.J-08viX3_VpEhkEg97VB0g',
+                maxZoom: 18,
               });
               break;
             case 'mapbox-satellite':
               var layer = L.tileLayer('https://a.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-                  attribution: '',
-                  id: 'mapbox.satellite',
-                  accessToken: 'pk.eyJ1IjoiZmFpcmUtc2F2b2lyIiwiYSI6ImNqcDQ3cTdqOTAxcGMzeG1ranV2NDlvb28ifQ.J-08viX3_VpEhkEg97VB0g',
-                  maxZoom: 18,
+                attribution: '',
+                id: 'mapbox.satellite',
+                accessToken: 'pk.eyJ1IjoiZmFpcmUtc2F2b2lyIiwiYSI6ImNqcDQ3cTdqOTAxcGMzeG1ranV2NDlvb28ifQ.J-08viX3_VpEhkEg97VB0g',
+                maxZoom: 18,
               });
               break;
             case 'osm':
               var layer = L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-                  attribution: '',
-                  maxZoom: 19
+                attribution: '',
+                maxZoom: 19
               });
               break;
           }
@@ -59,7 +59,7 @@ jQuery( window ).on( 'elementor/frontend/init', () => {
 
           var markers_array = [];
           var markers_group = L.featureGroup();
-          $(this).find('.point').each(function(idx, obj){
+          $(this).find('.point').each(function (idx, obj) {
 
             var marker_id = $(this).attr('data-id');
             var marker_lat = $(this).attr('data-lat');
@@ -67,21 +67,21 @@ jQuery( window ).on( 'elementor/frontend/init', () => {
             var marker_style = $(this).attr('data-marker');
 
 
-            if (marker_id !== '' && marker_lat !== '' && marker_lon !== ''){
+            if (marker_id !== '' && marker_lat !== '' && marker_lon !== '') {
               var popup = $(this).html()
 
               var iconDiv = L.divIcon({
-                iconSize:[48,58],
-                iconAnchor:[24,58],
-                popupAnchor:[0,-60],
-                className:'marker marker-id-'+marker_id+' '+((marker_style != '')?'marker-'+marker_style:''),
-                html:'<div class="pin"><span class="content">'+((marker_numbering == 'yes')?marker_id:'')+'<span></div>'
+                iconSize: [48, 58],
+                iconAnchor: [24, 58],
+                popupAnchor: [0, -60],
+                className: 'marker marker-id-' + marker_id + ' ' + ((marker_style != '') ? 'marker-' + marker_style : ''),
+                html: '<div class="pin"><span class="content">' + ((marker_numbering == 'yes') ? marker_id : '') + '<span></div>'
               })
 
-              var marker = new L.marker([marker_lat,marker_lon],{
-                icon : iconDiv,
+              var marker = new L.marker([marker_lat, marker_lon], {
+                icon: iconDiv,
               });
-              if (popup !== undefined){
+              if (popup !== undefined) {
                 marker.bindPopup(popup);
               }
               markers_array.push(marker);
@@ -90,8 +90,10 @@ jQuery( window ).on( 'elementor/frontend/init', () => {
           });
           elementorMap.addLayer(markers_group);
 
-          if(markers_array.length && fit_bounds == 'yes'){
-            elementorMap.fitBounds(markers_group.getBounds(),{padding: [50,50]});
+          if (markers_array.length && fit_bounds == 'yes') {
+            elementorMap.fitBounds(markers_group.getBounds(), {
+              padding: [50, 50]
+            });
           }
         }
       }
@@ -99,7 +101,7 @@ jQuery( window ).on( 'elementor/frontend/init', () => {
     });
   };
 
-  elementorFrontend.hooks.addAction( 'frontend/element_ready/fs-widget-leaflet-map.default', initialize_maps );
+  elementorFrontend.hooks.addAction('frontend/element_ready/fs-widget-leaflet-map.default', initialize_maps);
   elementorFrontend.hooks.addAction('frontend/element_ready/fs-widget-leaflet-map-tis.default', initialize_maps);
 
-} );
+});
